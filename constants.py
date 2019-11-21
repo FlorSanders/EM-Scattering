ebba8e59ce364d471eeadcbcf94886b0b3952b53
pdsmul <paul.desmul@ugent.be>
2019-11-21 15:33:18 +0100

    Source and Thing class, constants file

---
+++ constants.py
@@ -0,0 +1,5 @@
+import numpy as np
+eps_0 = 8.854 * 10**(-12) # [F/m]
+mu_0 = 4 * np.pi *  10**(-7) # [N/m^2]
+c = 299792458 # [m/s]
+simulation_speed_factor = 1 # reasonable range: between 1/2 and 2 higher = more accurate but slower
--- source.py
+++ source.py
@@ -1,9 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import special as sp
+import constants as cst
 
-def gaussian_pulse(t, J0, tc, sigma):
-    return J0*np.exp(-(t-tc)**2/(2*sigma**2))
+class Source:
+    
 
-def gaussian_modulated_rf_pulse(t, J0, tc, sigma, omega_c):
-    return gaussian_pulse(t, J0, tc, sigma)*np.sin(omega_c*t)
+    def _init_(self, pos_x, pos_y, J0, tc, sigma):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.J0 = J0 
+        self.tc = tc
+        self.sigma = sigma
+
+    def get_lambda_min(self,eps_r):
+        v_min = cst.c/np.sqrt(eps_r)
+        omega_max = 3/self.sigma * cst.simulation_speed_factor
+        return 2*np.pi * v_min / omega_max 
+
+
+class gaussian_pulse(Source):
+    def _init_(self, pos_x, pos_y, J0, tc, sigma):
+        Source.__init__(pos_x, pos_y, J0, tc, sigma)
+
+    def get_current(self,t)    :
+        return self.J0*np.exp(-(t-self.tc)**2/(2*self.sigma**2))
+
+class gaussian_modulated_rf_pulse(Source):
+    def _init_(self, pos_x, pos_y, J0, tc, sigma, omega_c):
+        Source.__init__(pos_x, pos_y, J0, tc, sigma)
+        self.omega_c = omega_c
+
+    def get_current(self,t):
+        return gaussian_pulse(t, self.J0, self.tc, self.sigma)*np.sin(self.omega_c*t)
