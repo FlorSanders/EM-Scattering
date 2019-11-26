68579741cb15237519664ba8f1713cf7a8b0f4a1
Thijs Paelman <thijs.paelman+git@hotmail.be>
2019-11-26 16:43:45 +0100

    non working inputscript

--- main.py
+++ main.py
@@ -3,48 +3,105 @@ import numpy as np
 from scipy import special as sp
 import space
 import dielectric
+
+# global dictionaries
+var_names = {}          # var names in simulation program
+var_description = {}    # var description to print
+questions = {}          # General question asked
+simulation = {}         # simulation dict: contains all usefull objects
+
 def tofloat():
     pass
 
 def question(questionstring, **kwargs):
+    """Prints out questionstring & returns output dict
+    asks for each key in kwargs a value
+    returns value associated with each key
+
+    Parameters
+    ----------
+    questionstring : str
+        The question to print
+    **kwargs
+        The keyword arguments are used to ask all parameters
+    Raises
+    ------
+    Returns
+    -------
+    output : dict
+        keys : **kwargs.keys()
+        values : input()
+    """
     output = {}
-    print("Please input a value for {}:".format(questionstring))
+    print(questionstring)
     for var, varstring in kwargs.items():
         output[var] = input(varstring + ":")
     return output
+
+
+def create_dielek(num_dielek):
+    """Makes space for 'num_dielek' dielectrics
+
+    In place operator on known data structures (var_... dicts)
+    Parameters
+    ----------
+    num_dielek : int
+        Number of dielectrics
+    Returns
+    -------
+        None
+    """
+    var_names["dielectric"] = [ "pos_x", "pos_y", "width", "height", "eps_r" ]
+    var_description["dielectric"] = [ "...", "...", "...", "...", "..." ]
+    questions["dielectric"] = []
+    simulation["dielectric"] = []
+    for i in range(num_dielek):
+        questions["dielectric"].append("What are the parameters of object {}".format(i))
+        simulation["dielectric"].append(dielectric.Dielectric)
+    return None
+
+
 def dialog():
-    # dict: simulation
-    simulation = {}
+    # iteration order of questions/subjects
+    keys = ["space", "num_dielek", "dielectric"]
+
+    ## All variable names, descriptions
     # space
-    var_names = [ "x_length", "y_length", "t_length"]
-    var_description = [ "the length of the simulation domain (units)", "the heigth of the simulation domain (units)", "the duration of the simulation (time units)"]
-    var_name_desc = { name: desc for name, desc in zip(var_names,var_description)}
-    dimensions = question("What are the dimensions of the simulation domain?", **var_name_desc)
-    dimensions = { k:int(v) for k,v in dimensions.items() }
-    simulation["space"] = space.Space(**dimensions)
+    var_names["space"] = [ "x_length", "y_length", "t_length"]
+    var_description["space"] = [ "the length of the simulation domain (units)", "the heigth of the simulation domain (units)", "the duration of the simulation (time units)"]
+    questions["space"] = ["What are the dimensions of the simulation domain?"]
+    simulation["space"] = [space.Space]
+
+    # number of objects
+    var_names["num_dielek"] = [ "num_dielek" ]
+    var_description["num_dielek"] = [ "Number of dielectric objects" ]
+    questions["num_dielek"] = ["Enter the number of dielectric"]
+    simulation["num_dielek"] = [create_dielek]
 
     # dielectric objects
-    simulation["objects"] = []
-    var_names = [ "pos_x", "pos_y", "width", "height", "eps_r" ]
-    var_description = [ "...", "...", "...", "...", "..." ]
-    var_name_desc = { name: desc for name, desc in zip(var_names,var_description)}
-    num_objs = int(question("number of objects", num_objs = "Number of objects")["num_objs"])
-    for i in range(num_objs):
-        obj_params = question("What are the parameters of object {}".format(i), **var_name_desc)
-        dielec_obj = dielectric.Dielectric(**obj_params)
-        simulation["objects"].append(dielec_obj)
+    # See create_dielek
 
     ## Line source
     # position
-    var_names = [ "pos_x", "pos_y" ]
-    var_description = [ "pos_x", "pos_y" ]
-    var_name_desc = { name: desc for name, desc in zip(var_names,var_description)}
-    pos = question("Position of line source", **var_name_desc)
-    pos = { k:int(v) for k,v in pos.items() }
+    var_names["line_source"] = [ "pos_x", "pos_y" ]
+    var_description["line_source"] = [ "pos_x", "pos_y" ]
+    questions["line_source"] = ["Position of line source"]
+
     # profile
-    var_names = [ "J0", "tc", "sigma" ]
-    var_description = var_names.copy()
-    var_name_desc = { name: desc for name, desc in zip(var_names,var_description)}
-    question("Profile line source", **var_name_desc)
+    #var_names = [ "J0", "tc", "sigma" ]
+    #var_description = var_names.copy()
+    #questions = ["Profile line source"]
+
+    for k in keys:
+        if not isinstance(questions[k], list):
+            questions[k] = [ questions[k] ]
+        for i, qst in enumerate(questions[k]):
+            var_name_desc = { name: desc for name, desc in zip(var_names[k],var_description[k])}
+            answer = question(qst, **var_name_desc)
+            answer = { k:int(v) for k,v in answer.items() }
+            simulation[k][i] = simulation[k][i](**answer)
+    simulation = { k:v for k, v in simulation.items() if v is not None }
+    print(simulation)
+
 
 dialog()
