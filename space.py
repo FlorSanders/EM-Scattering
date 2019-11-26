974fe8904b7e32df4826a936ceacd8c2fb37a95b
Thijs Paelman <thijs.paelman+git@hotmail.be>
2019-11-26 17:14:26 +0100

    minor adj

--- space.py
+++ space.py
@@ -27,9 +27,9 @@ class Space:
         self.Delta_t = Delta_t
 
         # Calculating the amount of space/time indices
-        self.N_x = int(self.x_length / Delta_x)
-        self.N_y = int(self.y_length / Delta_y)
-        self.N_t = int(self.t_length / Delta_t)
+        self.N_x = self.x_length // Delta_x + 1
+        self.N_y = self.y_length // Delta_y + 1
+        self.N_t = self.t_length // Delta_t # t_length not inclusive
 
         # Initializing zero-valued fields of the correct size (as 3D numpy array)
         # E_z field at the corners of our discretized blocks
@@ -50,15 +50,15 @@ class Space:
     def initialize_space(self, eps_averaging = True, plot_space = True):
         # Initialize space with relative permittivity (eps_r) 1 everywhere (vacuum)
         self.space = np.ones((self.N_x - 1, self.N_y - 1))
-        
+
         # Changing the relative permittivity according to position and size of our objects (assuming no overlap exists)
         for dielectric in self.dielectrics:
             # Discretizing given dimensions and positions
-            i = int(dielectric.pos_x / self.Delta_x)
-            j = int(dielectric.pos_y / self.Delta_y)
-            x_length = int(dielectric.width / self.Delta_x)
-            y_length = int(dielectric.height / self.Delta_y)
-            
+            i = dielectric.pos_x // self.Delta_x
+            j = dielectric.pos_y // self.Delta_y
+            x_length = dielectric.width // self.Delta_x
+            y_length = dielectric.height // self.Delta_y
+
             # Changing the value for eps_r at that position
             self.space[i:i + x_length, j:j + y_length] = dielectric.eps_r
         # Problem: The edges of the dielectric and the measurement points of E_z coincide, so eps_r is not well defined in this point
@@ -68,7 +68,7 @@ class Space:
         else:
             # Alternative solution: We shift the dielectrics slightly (1/2 step left- and downward) so we don't need to average values out.
             self.space = self.space[:-1, :-1]
-        
+
         # Visualizing our space using a plot
         if(plot_space):
             measurement.field_plot(self.space, "i", "j", "Visualisatie van de ruimte")
@@ -77,33 +77,33 @@ class Space:
     def FDTD(self, measurement_points, eps_averaging = True, visualize_space = False, make_animation = False):
         # Initialize the dielectric properties of the space
         self.initialize_space(eps_averaging)
-        
+
         # Calculating the discretized postions of our line source
-        i_source = int(self.source.pos_x / self.Delta_x)
-        j_source = int(self.source.pos_y / self.Delta_y)
+        i_source = self.source.pos_x // self.Delta_x
+        j_source = self.source.pos_y // self.Delta_y
         # if(make_animation):
         #     ims = []
         # Use the iterative update functions for our fields
-        for n in range(1, self.N_t): 
+        for n in range(1, self.N_t):
             # 1: Update H_y
             self.H_y[:, :, n] = self.H_y[:, :, n - 1]
             self.H_y[:, :, n] += self.Delta_t / (mu_0 * self.Delta_x) * (self.E_z[1:, :, n - 1] - self.E_z[:-1, :, n - 1])
-            
+
             # 2: Update H_x
             self.H_x[:, :, n] = self.H_x[:, :, n - 1]
             self.H_x[:, :, n] -= self.Delta_t / (mu_0 * self.Delta_y) * (self.E_z[:, 1:, n - 1] - self.E_z[:, :-1, n - 1])
-            
+
             # 3: Update E_z (inner space, edges = 0 as per boundary conditions)
-            self.E_z[:, :, n] = self.E_z[:, :, n - 1] 
+            self.E_z[:, :, n] = self.E_z[:, :, n - 1]
             self.E_z[1:-1, 1:-1, n] += self.Delta_t / (eps_0 * self.Delta_x) * (self.H_y[1:, 1:-1, n] - self.H_y[:-1, 1:-1, n]) / self.space
             self.E_z[1:-1, 1:-1, n] -= self.Delta_t / (eps_0 * self.Delta_y) * (self.H_x[1:-1, 1:, n] - self.H_x[1:-1, :-1, n]) / self.space
             self.E_z[i_source, j_source, n] -= self.source.get_current((n-1/2)*self.Delta_t) * self.Delta_t / (self.Delta_x * self.Delta_y * eps_0 * self.space[i_source, j_source])
-        
+
         # Making an animation
         if(make_animation):
             measurement.make_animation(self.E_z, name="ani_E_z")
             measurement.make_animation(np.sqrt(self.H_x[1:,:,:]**2 + self.H_y[:,1:,:]**2), name="ani_H_xy")
-        
+
         # Getting measurements
         measurements = []
         for point in measurement_points:
@@ -113,9 +113,10 @@ class Space:
             time_H = (np.arange(self.N_t) + 1/2) * self.Delta_t
             time_E = np.arange(self.N_t) * self.Delta_t
             # Slicing our matrix to obtain correct measurements (Lower bound approximation to our indices)
-            H_x = self.H_x[int(i), int(j), :]
-            H_y = self.H_y[int(i + 1/2), int(j + 1/2), :]
-            E_z = self.E_z[int(i + 1/2), int(j + 1/2), :]
+            # Assuming no i, j < 1/2
+            H_x = self.H_x[int(i - 1/2), int(j - 1/2), :]
+            H_y = self.H_y[int(i - 1/2), int(j - 1/2), :]
+            E_z = self.E_z[int(i), int(j), :]
             measure = measurement.Measurement(point[0], point[1], time_H, time_E, H_x, H_y, E_z)
             measurements.append(measure)
         # Returning the list of measurements
